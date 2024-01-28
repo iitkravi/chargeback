69c9969c7b8f36929f99b07eb6dcf769a05a7b09
iitkravi <ravi.cse.iitk@gmail.com>
2024-01-28 13:38:52 +0530

    Update code to show text for prediction instead 0/1

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import sklearn
 
 app = Flask(__name__)
 
-model = pickle.load(open('static/model.pkl', 'rb'))
+model= pickle.load(open('static/model.pkl', 'rb'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,13 +17,21 @@ def index():
         csv_data = request.form.get('csv_data', '')
         if csv_data:
             csv_file = StringIO(csv_data)
-            data = pd.read_csv(csv_file)  # Replace with your CSV file name
+            data = pd.read_csv("static/testdata.csv")  # Replace with your CSV file name
             data = data.fillna(0)  # Fill missing values with 0
             data = pd.get_dummies(data)
-            data = data.drop("target", axis=1)
+            x_data = data.drop("target", axis=1)
             print("Feature names during training:", model.feature_names_in_)
+            df = pd.DataFrame(columns=[model.feature_names_in_])
+            for col in df.columns:
+                print(col)
             print("Feature names during prediction:", data.columns)
-            result = model.predict(data)
+            x_data = pd.concat([df, x_data], ignore_index=True)
+            result = model.predict(x_data)
+            if result == 1:
+                result = 'This is a chargeback transaction'
+            else:
+                result = 'This is a normal transaction'
     return render_template('index.html', data=result)
 
 
